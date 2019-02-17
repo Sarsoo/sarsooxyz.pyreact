8de229bf9bfcdd8791743807e8b4274578ae0cce
aj <andrewjpack@gmail.com>
2019-02-17 10:04:54 +0000

    adding music page and profile

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/art')
-def art():
-    return render_template('art.html')
+@app.route('/music')
+def music():
+    return render_template('music.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
