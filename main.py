c8b6d4398d7eead6677fa54d25bca275e6fbdedf
aj <andrewjpack@gmail.com>
2019-02-19 14:11:06 +0000

    databased art displaying by tag working

--- main.py
+++ main.py
@@ -19,13 +19,30 @@ def music():
 @app.route('/art')
 def art():
     art_collection = db.collection(u'art')
-    #art_tags_collection = db.collection(u'art_tags')    
-    
+    art_tags_collection = db.collection(u'art_tags')    
+
     try:
         pics = art_collection.get()
-        #tags = art_tags_collection.get()
+        tags = art_tags_collection.get()
     except google.cloud.exceptions.NotFound:
         return 'no such document'
+
+    sections = []
+    
+    for tag in tags:
+        tag_dict = tag.to_dict()
+        query_ref = art_collection.where(u'tag', u'==', u'{}'.format(tag_dict['name']))
+        query = query_ref.get()
+
+        image_list = []
+            
+        for image in query:
+            image_list.append(image.to_dict())
+
+        #sections.append({tag_dict['name']: image_list})
+        sections.append({'images': image_list, 'name': tag_dict['name'], 'description': tag_dict['description'], 'index': tag_dict['index']})
+
+    print(sections[0])
 
     #categories = []
     #for tag in tags:
@@ -43,7 +60,7 @@ def art():
         images.append(doc.to_dict())
         #categories[categories.index(image['tag'])].append(image)
 
-    return render_template('art.html', staticroot = staticbucketurl, images=images)
+    return render_template('art.html', staticroot = staticbucketurl, images=images, tags=sections)
 
 @app.route('/dev')
 def dev():
