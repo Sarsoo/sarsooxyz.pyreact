1e353136a0e4f470c44eb3fa05771c32bb313800
aj <andrewjpack@gmail.com>
2019-03-02 18:08:28 +0000

    added last fm pulling

--- sarsoo/music/music.py
+++ sarsoo/music/music.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+import requests
 
 from google.cloud import firestore
 
 fs = firestore.Client()
 
 urlprefix = 'music'
+fm_url = 'https://ws.audioscrobbler.com/2.0/'
 
 music_print = Blueprint('music', __name__, template_folder='templates')
 
@@ -14,5 +16,21 @@ music_print = Blueprint('music', __name__, template_folder='templates')
 def root():
     fmkey = fs.document('key/fm').get().to_dict()['clientid']
     
+    params = {
+            'method':'user.gettopalbums',
+            'user':'sarsoo',
+            'period':'1month',
+            'limit':'6',
+            'api_key':fmkey,
+            'format':'json'
+            }
+
+    req = requests.get(fm_url, params = params)
+
+    albums = req.json()['topalbums']['album']
     
-    return render_template('music/index.html')
+    for album in albums:
+        for image in album['image']:
+            image['text'] = image['#text']
+
+    return render_template('music/index.html', albums = albums)
